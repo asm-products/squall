d73234aa59333884fc1c17f7dfe5f212bb46a15a
Karan Goel <karan@goel.im>
2014-12-15 10:56:49 -0800

    very primitive

--- script.js
+++ script.js
@@ -1,29 +1,150 @@
-var canvas = $("#e");
-var context = canvas.getContext("2d");
+var canvas = document.getElementById("e");
+var context = canvas.getContext('2d');
+var emmeasure = context.measureText("M").width;
+var spacemeasure = context.measureText(" ").width;
+
 var message = "";
 
+// $('#textBox').keyup(function(e) {
+//     drawString(context, e.target.value, 20, 30, '#333',0, 'Calibri', '16')
+// });
 
-$('#textBox').keydown(function(e) {
-    textBoxChanged(e);
-});
+document.getElementById('textBox').onkeyup = draw;
 
 
-function textBoxChanged(e) {
-    var target = e.target;
-    message = target.value;
-    drawScreen();
+// function textBoxChanged(e) {
+//     context.clearRect(0, 0, canvas.width, canvas.height);
+//     var target = e.target;
+//     message = target.value;
+//     // drawScreen();
+// }
+
+
+function draw() {
+    var lines = fragmentText(this.value, canvas.width * 0.9), // 10% padding
+        font_size = 22; // px
+    context.font = "Bold " + font_size + "px Arial";
+    context.save();
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    lines.forEach(function(line, i) {
+        context.fillText(line, 20, (i + 1) * font_size); // assume font height.
+    });
+    context.restore();
 }
 
 
-function drawScreen() {
-    context.clearRect (0, 0, canvas.width, canvas.height);
-    var metrics = context.measureText(message);
-    var textWidth = metrics.width;
-    var xPosition = (canvas.width / 2) - (textWidth / 2);
-    var yPosition = (canvas.height / 2);
+/* takes a string and a maxWidth and splits the text into lines */
+ // context is available in the parent scope.
+function fragmentText(text, maxWidth) {
+    if (maxWidth < emmeasure) // To prevent weird looping anamolies farther on.
+        throw "Can't fragment less than one character.";
 
-    context.fillText(message, xPosition, yPosition);
+    if (context.measureText(text).width < maxWidth) {
+        return [text];
+    }
 
-    var img = canvas.toDataURL("image/png");
-    $('#h').html('<img src="'+img+'"/>');
+    var words = text.split(' '),
+        metawords = [],
+        lines = [];
+
+    // measure first.
+    for (var w in words) {
+        var word = words[w];
+        var measure = context.measureText(word).width;
+
+        // Edge case - If the current word is too long for one line, break it into maximized pieces.
+        if (measure > maxWidth) {
+            // TODO - a divide and conquer method might be nicer.
+            var edgewords = (function(word, maxWidth) {
+                var wlen = word.length;
+                if (wlen == 0) return [];
+                if (wlen == 1) return [word];
+
+                var awords = [], cword = "", cmeasure = 0, letters = [];
+
+                // Measure each letter.
+                for (var l = 0; l < wlen; l++)
+                    letters.push({"letter":word[l], "measure":context.measureText(word[l]).width});
+
+                // Assemble the letters into words of maximized length.
+                for (var ml in letters) {
+                    var metaletter = letters[ml];
+
+                    if (cmeasure + metaletter.measure > maxWidth) {
+                        awords.push({ "word":cword, "len":cword.length, "measure":cmeasure });
+                        cword = "";
+                        cmeasure = 0;
+                    }
+
+                    cword += metaletter.letter;
+                    cmeasure += metaletter.measure;
+                }
+                // there will always be one more word to push.
+                awords.push({ "word":cword, "len":cword.length, "measure":cmeasure });
+                return awords;
+            })(word, maxWidth);
+
+            // could use metawords = metawords.concat(edgwords)
+            for (var ew in edgewords)
+                metawords.push(edgewords[ew]);
+        }
+        else {
+            metawords.push({ "word":word, "len":word.length, "measure":measure });
+        }
+    }
+
+    // build array of lines second.
+    var cline = "";
+    var cmeasure = 0;
+    for (var mw in metawords) {
+        var metaword = metawords[mw];
+
+        // If current word doesn't fit on current line, push the current line and start a new one.
+        // Unless (edge-case): this is a new line and the current word is one character.
+        if ((cmeasure + metaword.measure > maxWidth) && cmeasure > 0 && metaword.len > 1) {
+            lines.push(cline)
+            cline = "";
+            cmeasure = 0;
+        }
+
+        cline += metaword.word;
+        cmeasure += metaword.measure;
+
+        // If there's room, append a space, else push the current line and start a new one.
+        if (cmeasure + spacemeasure < maxWidth) {
+            cline += " ";
+            cmeasure += spacemeasure;
+        } else {
+            lines.push(cline)
+            cline = "";
+            cmeasure = 0;
+        }
+    }
+    if (cmeasure > 0)
+        lines.push(cline);
+
+    return lines;
 }
+
+// function drawScreen() {
+//     var words = message.split(' ');
+//     console.log(words);
+//     var line = '';
+
+//     for(var n = 0; n < words.length; n++) {
+//       var testLine = line + words[n] + ' ';
+//       var metrics = context.measureText(testLine);
+//       var testWidth = metrics.width;
+//       if (testWidth > maxWidth && n > 0) {
+//         context.fillText(line, x, y);
+//         line = words[n] + ' ';
+//         y += lineHeight;
+//       } else {
+//         line = testLine;
+//       }
+//     }
+//     context.fillText(line, x, y);
+
+//     var img = canvas.toDataURL("image/png");
+//     $('#h').html('<img src="'+img+'"/>');
+// }
