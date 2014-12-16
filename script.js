802d93d153ce00d2082c5906cd536b804e84078a
Karan Goel <karan@goel.im>
2014-12-16 11:03:29 -0800

    format the javascript

--- script.js
+++ script.js
@@ -1,121 +1,125 @@
 var canvas = document.getElementById("textCanvas");
 var context = canvas.getContext('2d');
+
 var emmeasure = context.measureText("M").width;
 var spacemeasure = context.measureText(" ").width;
 
 $('#textCanvas').attr('width', $('.panel-body').width());
 
 $('.textBox').keyup(function() {
-    draw();
+  draw();
 });
 window.onresize = draw;
 
 function draw() {
-    var lines = fragmentText($('.textBox').text(), canvas.width * 0.8),
-        font_size = 18;
-    $('#textCanvas').attr('height', lines.length * (font_size + 5) + 20);
-    context.font = font_size + "px sans-serif";
-    context.save();
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.textBaseline = 'top';
-    lines.forEach(function(line, i) {
-        console.log(line);
-        context.fillText(line, canvas.width * 0.1, (i + 1) * (font_size + 5));
-    });
-    document.getElementById('image').src = context.canvas.toDataURL();
-    context.restore();
+  var lines = fragmentText($('.textBox').text(), canvas.width * 0.8),
+      font_size = 18;
+  $('#textCanvas').attr('height', lines.length * (font_size + 5) + 20);
+  context.font = font_size + "px sans-serif";
+  context.save();
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.textBaseline = 'top';
+  lines.forEach(function(line, i) {
+    context.fillText(line, canvas.width * 0.1, (i + 1) * (font_size + 5));
+  });
+  document.getElementById('image').src = context.canvas.toDataURL();
+  context.restore();
 }
 
 
-/* takes a string and a maxWidth and splits the text into lines */
- // context is available in the parent scope.
 function fragmentText(text, maxWidth) {
-    if (maxWidth < emmeasure) // To prevent weird looping anamolies farther on.
-        throw "Can't fragment less than one character.";
+  if (maxWidth < emmeasure) {
+    throw "Can't fragment less than one character.";
+  }
 
-    if (context.measureText(text).width < maxWidth) {
-        return [text];
+  if (context.measureText(text).width < maxWidth) {
+    return [text];
+  }
+
+  var words = text.split(' '),
+      metawords = [],
+      lines = [];
+
+  for (var w in words) {
+    var word = words[w];
+    var measure = context.measureText(word).width;
+
+    var edgewords = (function(word, maxWidth) {
+      var wlen = word.length;
+      if (wlen == 0) return [];
+      if (wlen == 1) return [word];
+
+      var awords = [], cword = "", cmeasure = 0, letters = [];
+
+      for (var l = 0; l < wlen; l++) {
+        letters.push({
+          "letter": word[l],
+          "measure": context.measureText(word[l]).width
+        });
+      }
+
+      for (var ml in letters) {
+        var metaletter = letters[ml];
+
+        if (cmeasure + metaletter.measure > maxWidth) {
+          awords.push({
+            "word": cword,
+            "len": cword.length,
+            "measure": cmeasure
+          });
+          cword = "";
+          cmeasure = 0;
+        }
+
+        cword += metaletter.letter;
+        cmeasure += metaletter.measure;
+      }
+      awords.push({
+        "word": cword,
+        "len": cword.length,
+        "measure": cmeasure
+      });
+      return awords;
+    })(word, maxWidth);
+
+    for (var ew in edgewords) {
+      metawords.push(edgewords[ew]);
+    } else {
+      metawords.push({
+        "word": word,
+        "len": word.length,
+        "measure": measure
+      });
+    }
+  }
+
+  var cline = "";
+  var cmeasure = 0;
+  for (var mw in metawords) {
+    var metaword = metawords[mw];
+
+    if ((cmeasure + metaword.measure > maxWidth) &&
+         cmeasure > 0 && metaword.len > 1) {
+      lines.push(cline)
+      cline = "";
+      cmeasure = 0;
     }
 
-    var words = text.split(' '),
-        metawords = [],
-        lines = [];
+    cline += metaword.word;
+    cmeasure += metaword.measure;
 
-    // measure first.
-    for (var w in words) {
-        var word = words[w];
-        var measure = context.measureText(word).width;
-
-        // Edge case - If the current word is too long for one line, break it into maximized pieces.
-        if (measure > maxWidth) {
-            // TODO - a divide and conquer method might be nicer.
-            var edgewords = (function(word, maxWidth) {
-                var wlen = word.length;
-                if (wlen == 0) return [];
-                if (wlen == 1) return [word];
-
-                var awords = [], cword = "", cmeasure = 0, letters = [];
-
-                // Measure each letter.
-                for (var l = 0; l < wlen; l++)
-                    letters.push({"letter":word[l], "measure":context.measureText(word[l]).width});
-
-                // Assemble the letters into words of maximized length.
-                for (var ml in letters) {
-                    var metaletter = letters[ml];
-
-                    if (cmeasure + metaletter.measure > maxWidth) {
-                        awords.push({ "word":cword, "len":cword.length, "measure":cmeasure });
-                        cword = "";
-                        cmeasure = 0;
-                    }
-
-                    cword += metaletter.letter;
-                    cmeasure += metaletter.measure;
-                }
-                // there will always be one more word to push.
-                awords.push({ "word":cword, "len":cword.length, "measure":cmeasure });
-                return awords;
-            })(word, maxWidth);
-
-            // could use metawords = metawords.concat(edgwords)
-            for (var ew in edgewords)
-                metawords.push(edgewords[ew]);
-        }
-        else {
-            metawords.push({ "word":word, "len":word.length, "measure":measure });
-        }
+    if (cmeasure + spacemeasure < maxWidth) {
+      cline += " ";
+      cmeasure += spacemeasure;
+    } else {
+      lines.push(cline)
+      cline = "";
+      cmeasure = 0;
     }
+  }
+  if (cmeasure > 0) {
+    lines.push(cline);
+  }
 
-    // build array of lines second.
-    var cline = "";
-    var cmeasure = 0;
-    for (var mw in metawords) {
-        var metaword = metawords[mw];
-
-        // If current word doesn't fit on current line, push the current line and start a new one.
-        // Unless (edge-case): this is a new line and the current word is one character.
-        if ((cmeasure + metaword.measure > maxWidth) && cmeasure > 0 && metaword.len > 1) {
-            lines.push(cline)
-            cline = "";
-            cmeasure = 0;
-        }
-
-        cline += metaword.word;
-        cmeasure += metaword.measure;
-
-        // If there's room, append a space, else push the current line and start a new one.
-        if (cmeasure + spacemeasure < maxWidth) {
-            cline += " ";
-            cmeasure += spacemeasure;
-        } else {
-            lines.push(cline)
-            cline = "";
-            cmeasure = 0;
-        }
-    }
-    if (cmeasure > 0)
-        lines.push(cline);
-
-    return lines;
+  return lines;
 }
