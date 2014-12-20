429abefb357d0c62b2a31d18d835b60547422862
Karan Goel <karan@goel.im>
2014-12-19 17:23:49 -0800

    stupid bug

--- models/users.js
+++ models/users.js
@@ -15,7 +15,7 @@ var userSchema = new Schema({
   photo: String,
   access_token: String,
   access_token_secret: String,
-  tweet_ids = []
+  tweet_ids: []
 });
 
 module.exports = mongoose.model('Users', userSchema);
