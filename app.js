acfda51a505026aaa5adf0ff20b28bd33e53b0bc
Karan Goel <karan@goel.im>
2014-12-22 15:51:33 -0800

    truly no limits, close #13

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
+app.use(bodyParser({ limit: '5mb' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
