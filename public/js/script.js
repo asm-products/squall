0fff316a59199b531139a0771c99707ff4c2c4df
Nasser Al-Wohaibi <noonon@gmail.com>
2015-03-10 23:55:03 +0300

    Revert "fixing pull conflicts"
    This reverts commit 04275aafe6fb53105dd60ae6ef671244dbaedb65.

--- public/js/script.js
+++ public/js/script.js
@@ -220,7 +220,7 @@ $('#textArea').keyup(function() {
   var text = $(this).val();
   var splits = text.split(' ');
   geturl = new RegExp("(^|[ \t\r\n])((ftp|http|https|gopher|mailto|news|nntp|telnet|wais|file|prospero|aim|webcal):(([A-Za-z0-9$_.+!*(),;/?:@&~=-])|%[A-Fa-f0-9]{2}){2,}(#([a-zA-Z0-9][a-zA-Z0-9$_.+!*(),;/?:@&~=%-]*))?([A-Za-z0-9$_+!*();/?:~-]))", "g");
-  var length = (text.match(/ /g) || []).length;
+  var length = 0;
 
   for (var i = 0; i < splits.length; i++) {
     if (splits[i].match(geturl) && splits[i].length > TCO_LENGTH) {
