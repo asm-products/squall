68af9dec8929fc1b9460b4ce3e027c8a5349b07e
philipmoniaga <philip.moniaga@gdplabs.id>
2015-03-31 19:51:26 +0700

    Update setting router sycn with master

--- routes/web/settings.js
+++ routes/web/settings.js
@@ -6,22 +6,26 @@ module.exports = function(app, Users) {
     }); 
 
     app.post('/settings', app.auth.isAuthenticated, function(req, res) {
-        req.user.email_address = req.body.email;
-        req.user.alert_when_friends_join = req.body.alert_when_friends_join;
-        req.user.alert_when_follow = req.body.alert_when_follow;
 
-    var new_username = req.body.username;
+        var params = req.body;
+        req.user.email_address = params.user.email_address;
+        req.user.alert_when_friends_join = params.user.alert_when_friends_join;
+        req.user.alert_when_follow = params.user.alert_when_follow;
+
+    if (params.changeUsername) {
+    var new_username = params.user.username;
 
     if (!new_username) {
-    return res.json({errors: ['Username cannot be empty.']});
+      return res.json({errors: ['Username cannot be empty.']});
     }
 
     if (req.user.old_username) {
-    return res.json({errors: ['Username cannot be changed more than once.']});
+      return res.json({errors: ['Username cannot be changed more than once.']});
     }
 
     if (new_username === req.user.username) {
-    return res.json({errors: ['New Username cannot be same as existing username.']});
+      return res.json({errors: ['New Username cannot be same as existing username.']});
+    }n res.json({errors: ['New Username cannot be same as existing username.']});
     }
 
     // make sure new username is available
